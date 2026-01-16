f161685a48e614d084b45b5845dffb212a66cdb1
opensaichat <opensaichat@gmail.com>
2026-01-16 20:36:56 +0300

    Update Chat.js

--- Chat.js
+++ Chat.js
@@ -1,1 +1,22 @@
+const messages = document.getElementById('messages');
+const input = document.getElementById('input');
+const user = localStorage.getItem('user') || 'Гость';
 
+function send(){
+  if(!input.value) return;
+
+  addMessage(user + ': ' + input.value);
+  setTimeout(() => {
+    addMessage('NeuroAI: Я пока в разработке 🤖');
+  }, 600);
+
+  input.value = '';
+}
+
+function addMessage(text){
+  const div = document.createElement('div');
+  div.className = 'msg';
+  div.innerText = text;
+  messages.appendChild(div);
+  messages.scrollTop = messages.scrollHeight;
+}
